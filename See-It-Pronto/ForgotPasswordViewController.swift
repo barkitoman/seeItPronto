dc6029f600d39feb6e0b255269f65993e841c6c0
Deyson <deyson.bejarano@nyxent.com>
2016-01-04 15:04:53 -0500

    terminadas las vistas y adicionados todos los controllers para cada una de las vistas

---
+++ See-It-Pronto/ForgotPasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ForgotPasswordViewController.swift
+//  See-It-Pronto
+//
+//  Created by user114136 on 1/4/16.
+//  Copyright © 2016 user114136. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ See-It-Pronto/RealtorForm1ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RealtorForm1ViewController.swift
+//  See-It-Pronto
+//
+//  Created by user114136 on 1/4/16.
+//  Copyright © 2016 user114136. All rights reserved.
+//
+
+import UIKit
+
+class RealtorForm1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        navigationController?.navigationBarHidden = true
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        if (navigationController?.topViewController != self) {
+            navigationController?.navigationBarHidden = false
+        }
+        super.viewWillDisappear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnBack(sender: AnyObject) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
