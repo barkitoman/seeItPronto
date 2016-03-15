744fe647f0c4176ed07d430cd8c9f820acb706c7
Deyson <deyson.bejarano@nyxent.com>
2016-03-15 08:59:00 -0500

    start appoiment list for realtor user

---
+++ See-It-Pronto/MyListingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyListingsTableViewCell.swift
+//  See-It-Pronto
+//
+//  Created by Deyson on 3/14/16.
+//  Copyright © 2016 user114136. All rights reserved.
+//
+
+import UIKit
+
+class MyListingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
